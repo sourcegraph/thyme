ed20c31621f284e26c808a0fd2acb2695637959b
Beyang Liu <beyang@sourcegraph.com>
2016-08-07 18:43:56 -0700

    show coarse- and fine-grained timeline

--- show.go
+++ show.go
@@ -88,8 +88,24 @@ func NewTimeline(stream *Stream, labelFunc func(*Window) string) *Timeline {
 }
 
 func Stats(stream *Stream) error {
-	stats := NewTimeline(stream, func(w *Window) string { return w.Name })
-	if err := statsTmpl.Execute(os.Stdout, &stats); err != nil {
+	tlFine := NewTimeline(stream, func(w *Window) string { return w.Name })
+	tlCoarse := NewTimeline(stream, func(w *Window) string {
+		if w.Info().App != "" {
+			return w.Info().App
+		}
+		if w.Info().SubApp != "" {
+			return fmt.Sprintf("%s :: %s", w.Info().App, w.Info().SubApp)
+		}
+		if w.Info().Title != "" {
+			return fmt.Sprintf("%s :: %s :: %s", w.Info().App, w.Info().SubApp, w.Info().Title)
+		}
+		return w.Name
+	})
+
+	if err := statsTmpl.Execute(os.Stdout, &statsPage{
+		Fine:   tlFine,
+		Coarse: tlCoarse,
+	}); err != nil {
 		return err
 	}
 	return nil
@@ -99,6 +115,11 @@ func timeToJS(t time.Time) string {
 	return fmt.Sprintf(`new Date(%d, %d, %d, %d, %d, %d)`, t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
 }
 
+type statsPage struct {
+	Fine   *Timeline
+	Coarse *Timeline
+}
+
 var statsTmpl = template.Must(template.New("").Funcs(map[string]interface{}{
 	"timeToJS": timeToJS,
 }).Parse(`<html>
@@ -106,9 +127,13 @@ var statsTmpl = template.Must(template.New("").Funcs(map[string]interface{}{
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
       google.charts.load('current', {'packages':['timeline']});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var container = document.getElementById('timeline');
+	</script>
+
+	{{with .Coarse}}
+    <script type="text/javascript">
+      google.charts.setOnLoadCallback(drawChartCoarse);
+      function drawChartCoarse() {
+        var container = document.getElementById('timeline_coarse');
         var chart = new google.visualization.Timeline(container);
         var dataTable = new google.visualization.DataTable();
 
@@ -149,9 +174,69 @@ var statsTmpl = template.Must(template.New("").Funcs(map[string]interface{}{
         chart.draw(dataTable, options);
       }
     </script>
+	{{end}}
+
+	{{with .Fine}}
+    <script type="text/javascript">
+      google.charts.setOnLoadCallback(drawChartFine);
+      function drawChartFine() {
+        var container = document.getElementById('timeline_fine');
+        var chart = new google.visualization.Timeline(container);
+        var dataTable = new google.visualization.DataTable();
+
+        dataTable.addColumn({ type: 'string', id: 'Status' });
+		dataTable.addColumn({ type: 'string', id: 'Name' });
+        dataTable.addColumn({ type: 'date', id: 'Start' });
+        dataTable.addColumn({ type: 'date', id: 'End' });
+        dataTable.addRows([
+		{{range .Rows.Active}}
+			[
+				"Active",
+				"{{.Label}}",
+				{{timeToJS .Start}},
+				{{timeToJS .End}},
+			],
+		{{end}}
+		{{range .Rows.Visible}}
+			[
+				"Visible",
+				"{{.Label}}",
+				{{timeToJS .Start}},
+				{{timeToJS .End}},
+			],
+		{{end}}
+		{{range .Rows.All}}
+			[
+				"All",
+				"{{.Label}}",
+				{{timeToJS .Start}},
+				{{timeToJS .End}},
+			],
+		{{end}}
+		]);
+
+		var options = {
+			timeline: { showRowLabels: true },
+		};
+        chart.draw(dataTable, options);
+      }
+    </script>
+	{{end}}
+
+
   </head>
   <body>
-    <div id="timeline" style="height: 100%;"></div>
+
+	<div>
+		This is a coarse-grained chart of all the applications you over the course of the day. Every bar represents an application.
+	</div>
+    <div id="timeline_coarse" style="min-height: 500px;"></div>
+
+	<div>
+		This is a fine-grained chart of all the applications you over the course of the day. Every bar represents a distinct window.
+	</div>
+    <div id="timeline_fine" style="min-height: 500px;"></div>
+
   </body>
 </html>`))
 
