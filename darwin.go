6d60252dc3c27c54bbc00773524aed3415cfe5e9
CarolineFrost <cfrost16@stanford.edu>
2016-08-15 15:56:23 -0700

    changed setup directions for mac

--- darwin.go
+++ darwin.go
@@ -88,7 +88,7 @@ end tell
 
 func (t *DarwinTracker) Deps() string {
 	return `
-No external dependencies are required, but you do need to enable privileges for "System Events" in System Preferences > Security & Privacy > Accessibility.
+No external dependencies are required, but you do need to enable privileges for "System Events" in System Preferences > Security & Privacy > Privacy > Accessibility.
 See https://support.apple.com/en-us/HT202802 for details.
 `
 }
